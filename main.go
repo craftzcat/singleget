f7646190cb29912b516b03f173137982e9a2936f
craftzcat <umibooose@gmail.com>
2020-10-18 16:43:26 +0900

    [3.4] Display status on the console

--- main.go
+++ main.go
@@ -12,6 +12,11 @@ func main() {
 		panic(err)
 	}
 
+	// Display as a string. (e.g., "200 OK")
+	log.Println("Staus:", resp.Status)
+	// Display as an integer. (e.g., 200)
+	log.Println("StausCode:", resp.StatusCode)
+
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
