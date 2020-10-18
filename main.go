74bc5d9c123a43fb2fbec0a0961e3da399346594
craftzcat <umibooose@gmail.com>
2020-10-18 16:48:45 +0900

    [3.4] Add comments

--- main.go
+++ main.go
@@ -18,10 +18,11 @@ func main() {
 	log.Println("StausCode:", resp.StatusCode)
 
 	log.Println("Headers:", resp.Header)
-	// Display a specific header
+	// Display a specific header.
 	log.Println("Content-Length:", resp.Header.Get("Content-Length"))
 
 	defer resp.Body.Close()
+	// Read the contents of io.Reader into a byte array.
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		panic(err)
